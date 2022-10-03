4aaefd0c55f30521aa40bc428edfeb3a7c1e1289
Bagalov Shamil <thwglobal2000@gmail.com>
2022-10-03 18:04:26 +0300

    add translate google to pages show vacabcy resume company

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
-
+@section('scripts')
+    @parent
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+@endsection
 @section('content')
     <company-component
         :lang="{{json_encode($lang)}}"
--- resources/views/vacancies/show_vacancy.blade.php
+++ resources/views/vacancies/show_vacancy.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 {{--@extends('layouts.show-document')--}}
-
+@section('scripts')
+    @parent
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+@endsection
 @section('content')
     <show-vacancy-component
         :lang="{{json_encode($lang)}}"
